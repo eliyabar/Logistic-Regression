5abe5661f7afe996fb14e0800790694d3ff03b55
elya-PC\elya <flashxyz@gmail.com>
2017-08-22 00:31:16 +0300

    split data to train and test

--- logistic_regression.py
+++ logistic_regression.py
@@ -42,16 +42,41 @@ def menu_run_logistic():
     # get data from CSV
     x_df = load_data_from_csv_file("bank.csv")
 
+    x_df, y_df = prepare_data(x_df)
+
+    length_of_train = round(len(x_df.index)*0.8)
+
+    x_train, y_train = x_df[:length_of_train], y_df[:length_of_train]
+    x_test, y_test = x_df[length_of_train:], y_df[length_of_train:]
+
+    # y_df = np.ravel(load_data_from_csv_file("y_data.csv"))
+
+    # print(list(x_df))
+    # print(y_df)
+    # print(x_df)
+    logistic_object = LogisticRegression(solver='liblinear')
+    logistic_object.fit(x_df, y_df)
+    score = logistic_object.score(x_train, y_train)
+    print("score: " + str(score))
+
+    print(sum(logistic_object.predict(x_test) == y_test), "out of: ", len(y_test.index))
+    # logistic_object = LogisticRegression(class_weight={1: 0.9, 0: 0.1})
+    # logistic_object = LogisticRegression(solver='lbfgs')
+    # logistic_object.fit(x_df, y_df, sample_weight=sample_weight)
+    # score = logistic_object.score(x_df, y_df)
+    #
+    # print("score: " + str(score))
+
+
+def prepare_data(x_df):
     # replace yes/no with boolean
     str_to_boolean = {'yes': 1, 'no': 0}
     x_df['housing'] = x_df['housing'].map(str_to_boolean)
     x_df['loan'] = x_df['loan'].map(str_to_boolean)
     x_df['default'] = x_df['default'].map(str_to_boolean)
     x_df['y'] = x_df['y'].map(str_to_boolean)
-
     # split y out of the DataFrame
     y_df = x_df['y']
-
     print(x_df)
     # make dummies out of textual column
     dummy1 = pd.get_dummies(x_df['marital'], prefix='marital')
@@ -60,7 +85,6 @@ def menu_run_logistic():
     dummy4 = pd.get_dummies(x_df['contact'], prefix='contact')
     dummy5 = pd.get_dummies(x_df['month'], prefix='month')
     dummy6 = pd.get_dummies(x_df['poutcome'], prefix='poutcome')
-
     # Convert to int
     dummy1 = dummy1.astype(dtype='int32')
     dummy2 = dummy2.astype(dtype='int32')
@@ -68,27 +92,12 @@ def menu_run_logistic():
     dummy4 = dummy4.astype(dtype='int32')
     dummy5 = dummy5.astype(dtype='int32')
     dummy6 = dummy6.astype(dtype='int32')
-
     dummies_list = [dummy1, dummy2, dummy3, dummy4, dummy5, dummy6]
-
     # remove column from original df
     x_df = x_df.drop(['marital', 'job', 'education', 'contact', 'month', 'poutcome', 'y'], 1)
     # Join both parts
     x_df = x_df.join(dummies_list)
-
-    # y_df = np.ravel(load_data_from_csv_file("y_data.csv"))
-
-    print(list(x_df))
-    print(y_df)
-    # print(x_df)
-
-
-    # logistic_object = LogisticRegression(class_weight={1: 0.9, 0: 0.1})
-    # logistic_object = LogisticRegression(solver='lbfgs')
-    # logistic_object.fit(x_df, y_df, sample_weight=sample_weight)
-    # score = logistic_object.score(x_df, y_df)
-    #
-    # print("score: " + str(score))
+    return x_df, y_df
 
 
 # =======================
